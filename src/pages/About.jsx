ef4d45c10010aedec69d3ead7b2e44e7e802991f
0Gibbonator0 <orionmaster563@gmail.com>
2025-08-26 13:40:50 -0700

    Started working on About page and adding boxes with text (along with a title), finished first box, but needs improvement and organization for a moreprofessional look.

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,10 +3,19 @@ import "../App.css";
 export default function About() {
   return (
     <div className="page page--about">
-      <div className="center">
-        <h1>About Page</h1>
-        <p className="mt-2">This is where you can talk about yourself or your project.</p>
+      <div className = "text-box-left">
+        <p className = "mt-3">
+          My full name is Arshvir Sandhu. I am currently a sophmore student at San Diego State University
+          studying Computer Engineering. After my freshman year, I decided to get ahead and start working on leetcode
+          and coding projects on the side. I had the idea that I needed to start building an impressive resume and work
+          on problem-solving skills through leetcode. I got interested in tech and programming since highschool
+          and taking my first Computer Science class. In addition to that, I've always loved computers and playing
+          video games which sparked my curiosity into how all these things were organized and made. As of right
+          now I have developed skills in the following languages: Python, Java, C, C++ with my strongest being Python.
+          I also have experience working with React + Vite and solving Leetcode problems.
+        </p>
       </div>
+        <h1 className = "about-top-left">Background & Skills</h1>
     </div>
   );
 }
